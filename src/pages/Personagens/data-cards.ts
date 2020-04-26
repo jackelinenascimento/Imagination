da88b116f5bc9c6fa820fab574d4be25e02ba29c
Jackeline do Nascimento <jackeline_nascimento@hotmail.com>
2020-04-26 13:04:28 -0300

    added other personagens

--- src/pages/Personagens/data-cards.ts
+++ src/pages/Personagens/data-cards.ts
@@ -1,6 +1,24 @@
+import shurikenger01 from './images/shurikenger01/foto4.jpg'
+
 import peter01 from './images/homemAranha01/peter01.jpg'
 
+import tk01 from './images/tk01/tk01.jpg'
+
 const cards = [
+    {
+        key: 3,
+        imagemCard: shurikenger01,
+        tituloCard: "Shurikenger- O Ninja e a Honra",
+        textoCard: "Série Ninpuu Sentai Hurricanger.",
+        idLink: "#shurikenger01"
+    },
+    {
+        key: 2,
+        imagemCard: tk01,
+        tituloCard: "TK e Esperança",
+        textoCard: "Lições que esses dois personagens nos dão.",
+        idLink: "#tk01"
+    },
     {
         key: 1,
         imagemCard: peter01,
--- src/pages/Personagens/data-text.ts
+++ src/pages/Personagens/data-text.ts
@@ -1,3 +1,13 @@
+import shurikenger01 from './images/shurikenger01/foto1.jpg'
+import shurikenger02 from './images/shurikenger01/foto2.jpg'
+import shurikenger03 from './images/shurikenger01/foto3.jpg'
+import shurikenger04 from './images/shurikenger01/foto4.jpg'
+
+import tk01 from './images/tk01/tk01.jpg'
+import tk02 from './images/tk01/tk02.jpg'
+import tk03 from './images/tk01/tk03.jpg'
+import tk04 from './images/tk01/tk04.jpg'
+
 import peter01 from './images/homemAranha01/peter01.jpg'
 import peter02 from './images/homemAranha01/peter02.jpg'
 import peter03 from './images/homemAranha01/peter03.jpg'
@@ -5,9 +15,39 @@ import peter04 from './images/homemAranha01/peter04.jpg'
 
 const text = [
     {
+        key: 3,
+        id: "shurikenger01",
+        titulo: "Shurikenger- O Ninja e a Honra",
+        paragrafo1: "Na série Ninpuu Sentai Hurricanger de 2002 que vi e gostei muito. O que mais me chamou a atenção foi o personagem Shurikenger, um ninja que abre mão de sua verdadeira identidade para servir. Isso mostra muito do espirito japonês em samurais e ninjas que dura até hoje. A importância em ser leal a um senhor, a uma causa ou nesse caso a Gozei-SAMA (uma entidade antiga na série).",
+        image1: shurikenger01,
+        imagemDescricao1: "O ninja da escola dos espaço Shurikenger",
+        paragrafo4: "Mesmo não tendo seu rosto revelado em nenhum momento, ainda sim da pra perceber muito da sua personalidade e é um dos mais otimistas e alegres do grupo. E nos momentos decisivos mostrou que um ninja deve sempre olhar para frente, nunca para trás, muito menos lamentar os erros e sim focar como vencer no futuro.",
+        image2: shurikenger02,
+        imagemDescricao2: "A primeira cena do ninja espacial",
+        paragrafo7: "Era o mais experiente guerreiro com diversas técnicas e a calma de um verdadeiro ninja. Claro que os diferentes disfarces (que rendem ótimas participações especiais de atores conhecidos) e o seu jogo de palavras com japonês e inglês são um atrativo a mais para o seu carisma e destaque.  O Ninja misterioso é um exemplo de foco e força de vontade e também que mesmo na derrota não se deixou abalar e conseguiu levantar uma vez mais para a luta! Tenkuu Shinobi Change! Shurikenger da escola de ninjas do espaço!",
+        image3: shurikenger03,
+        imagemDescricao3: "Técnica do baiseball"
+    },
+    {
+        key: 2,
+        id: "tk01",
+        titulo: "TK e a Esperança",
+        paragrafo1: "A esperança se não é o maior na certa é um dos maiores sentimentos que o seres humanos possuem, mesmo em desvantagem, o poder que a esperança tem é enorme! A capacidade de vencer e superar qualquer situação!",
+        paragrafo2: "E é exatamente isso que o Takaishi Takeru, o TK de Digimon representa tão bem, meu personagem preferido no anime, ao lado de seu Digimon o Patamon. Na primeira temporada ele é o mais novo dos escolhidos e considerado como o mais fraco e vai crescendo ao longo da trama, tem conflitos com seu parceiro digimon, com o irmão e com os seus amigos.",
+        image1: tk01,
+        imagemDescricao1: "",
+        paragrafo4: "Mas isso o desenvolve o tornando forte e confiante para ser sempre o último elo de defesa. Isso tanto na primeira aparição de Angemon contra Devilmon a qual o primeiro encontro também é uma despedida, já que Angemon se sacrifica, mas volta como um  digiovo e aprende tudo novamente.",
+        paragrafo5: "Assim como no final da primeira temporada onde  Piedmon vence todos os outros escolhidos, TK  é encorajado por Matt  e lembra de não desistir e esse é o seu maior poder, não deixar as trevas e o medo tomarem conta. E claro que esse sentimento desperta o poder do brasão da Esperança e surge o Holly Angemon  salvando todos do feitiço.",
+        image2: tk02,
+        imagemDescricao2: "",
+        paragrafo7: "Na segunda temporada já mais velho ele amadurece ainda mais e sempre sendo otimista  e carregando a responsabilidade de ter mais experiência no grupo dos novos digi-escolhidos.",
+        paragrafo8: "O final de Digimon 02  é revelado que a voz do narrador que nos levava as aventuras fantásticas do digimundo são as memórias e sentimentos do escolhido da esperança. Fica muito marcante e da um arrepio quando ele fecha o livro.  TK nos mostrou a força da esperança e que não podemos desistir desse sentimento, mesmo enfrentando os maiores monstros e mesmo quando achamos que estamos sozinhos!",
+        image3: tk03
+    },
+    {
         key: 1,
         id: "peter01",
-        titulo: "Peter Parker / Homem Aranha",
+        titulo: "Peter Parker, o Homem Aranha",
         paragrafo1: "Vou começar falando de um dos personagens que mais admiro criados por Stan Lee:",
         paragrafo2: "Peter Parker, o homem aranha foi o primeiro quadrinho que tive em mãos, presente de um  conhecido quando eu tinha 8 anos,  nele Peter tinha que correr para se encontrar com Mary Jane, mas se atrasa porque teve que lutar com o doutor Octopus.",
         paragrafo3: "Depois li outros quadrinhos, mas o homem aranha sempre me cativou, sempre assistia o desenho que passava na globo, que pra mim é a melhor adaptação do herói  e seu universo até hoje. O que Peter Parker tem de especial? Ele carrega o fardo de ter falhado com seu tio, de ter sido egoísta e isso teve consequências e a frase “Com grandes poderes vem grandes responsabilidades” em sua mente.",
